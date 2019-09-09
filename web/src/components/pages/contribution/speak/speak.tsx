3d64d4a8d2ffcdb83a2e5e929be5b26e0391cb63
Abdulrahman Sulaiman <amansulaiman92@gmail.com>
2019-09-09 09:52:54 +0100

    design changes

--- web/src/components/pages/contribution/speak/speak.tsx
+++ web/src/components/pages/contribution/speak/speak.tsx
@@ -42,7 +42,7 @@ import { SentenceRecording } from './sentence-recording';
 import './speak.css';
 
 const MIN_RECORDING_MS = 1000;
-const MAX_RECORDING_MS = 10000;
+const MAX_RECORDING_MS = 12000;
 const MIN_VOLUME = 1;
 
 enum RecordingError {
